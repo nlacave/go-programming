e0ca0e748a3e304c644425494c83da243081faed
Nicolas Lacave <Nicolas.Lacave@belltech.la>
2024-03-26 10:30:33 -0300

    Generando documentación

---
+++ Ejercicio_7/mymath/main.go
@@ -0,0 +1,11 @@
+// Package mymath provides math solutions
+package mymath
+
+//Sum suma un numero ilimitado de valores de tipo int
+func Sum(xi ...int) int {
+	sum := 0
+	for v := range xi {
+		sum += v
+	}
+	return sum
+}
